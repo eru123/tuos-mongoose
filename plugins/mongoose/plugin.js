1db30492286f906e08926f207b257dcc854366e4
eru123 <yeoligoakino@gmail.com>
2021-09-07 13:42:16 +0800

    added: paginate

--- plugins/mongoose/plugin.js
+++ plugins/mongoose/plugin.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const fp = require('fastify-plugin')
+const paginate = require('mongoose-paginate-v2')
 
 const plugin = async function(fastify, options, done) {
 
@@ -11,28 +12,23 @@ const plugin = async function(fastify, options, done) {
     useUnifiedTopology: true
   } 
 
-  if(!('mongoose' in options)) throw new Error(`[PLUGIN] mongoose: 'options.mongoose' is not defined`)
+  if(!('mongoose' in options)) throw new Error(`'options.mongoose' is not defined`)
 
   const connect = options.mongoose.connect || 'mongodb://localhost:27017/tuos'
   const opts = options.mongoose.options || defaultOpts 
 
-  process.on('unhandledRejection', e => {
-    console.error('[PLUGIN] mongoose: Connect ERROR: Timeout')
-    process.exit(1)
-  });
+  mongoose.plugin(paginate)
 
   await mongoose.connect(connect, opts)
-    .then(() => {
+    .then((e) => {
       console.log('[PLUGIN] mongoose: Connected.')
-      process.on('unhandledRejection', () => null);
+      fastify.decorate('mongoose', e)
     })
     .catch(e => {
       console.error('[PLUGIN] mongoose: Connect ERROR:', e.message)
       process.exit(1)
     })
-
-  fastify.decorate('mongoose', mongoose)
-
+    
   done()
 }
 
